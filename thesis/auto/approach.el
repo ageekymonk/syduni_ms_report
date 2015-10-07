34030e103b3f1a54e2ad4ae1fa792855a83edd5e
ramz <ramzthecoder@gmail.com>
2015-10-07 19:01:37 +1100

    A3 initial commit

---
+++ thesis/auto/approach.el
@@ -0,0 +1,22 @@
+(TeX-add-style-hook
+ "approach"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("IEEEtran" "conference")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("graphicx" "pdftex")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "IEEEtran"
+    "IEEEtran10"
+    "graphicx"
+    "amssymb"
+    "algorithmic"
+    "algorithm")
+   (LaTeX-add-labels
+    "notations"
+    "algo_pivot_selection"
+    "algo_join")
+   (LaTeX-add-bibliographies
+    "knnjoin")))
+
