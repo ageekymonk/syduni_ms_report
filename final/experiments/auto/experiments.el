4c49ca56e30daba3a5dc0795c54ddb15759ac014
ramz <ramzthecoder@gmail.com>
2015-11-03 22:53:02 +1100

    More changes in results

--- final/experiments/auto/experiments.el
+++ final/experiments/auto/experiments.el
@@ -5,5 +5,17 @@
     "chap:Evaluation and Results"
     "fig_effect_of_dataset_size_1"
     "fig_effect_of_dataset_size_2"
-    "fig_effect_of_dataset_size")))
+    "fig_effect_of_dataset_size"
+    "fig_effect_of_k_1"
+    "fig_effect_of_k_2"
+    "fig_effect_of_k"
+    "fig_effect_of_num_pivots_1"
+    "fig_effect_of_num_pivots_2"
+    "fig_effect_of_num_pivots"
+    "fig_effect_of_num_dim_1"
+    "fig_effect_of_num_dim_2"
+    "fig_effect_of_num_dim"
+    "fig_effect_of_num_nodes_1"
+    "fig_effect_of_num_nodes_2"
+    "fig_effect_of_num_nodes")))
 
