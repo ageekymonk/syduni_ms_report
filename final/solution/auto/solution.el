97d5e06561d20faacad96cec3ff85c1178c01305
ramz <ramzthecoder@gmail.com>
2015-11-06 16:45:28 +1100

    Adding all the algos

--- final/solution/auto/solution.el
+++ final/solution/auto/solution.el
@@ -4,5 +4,9 @@
    (LaTeX-add-labels
     "algo_pivot_selection"
     "algo_dataset_partition"
+    "algo_generate_summary"
+    "algo_bruteforce_nn"
+    "fig_triangle_inequality"
+    "algo_nbr_partition"
     "algo_join")))
 
